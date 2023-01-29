fbb1d3e493ec509cf49f5bc5585bc1cd007d7036
Alexander_Ablaza <alexanderxablaza@gmail.com>
2023-01-29 19:19:13 +0800

    LList

--- javastudy/src/io/collections/llist.java
+++ javastudy/src/io/collections/llist.java
@@ -1,7 +1,20 @@
 package io.collections;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class llist {
     public static void main(String[] args) {
-        
+        LinkedList<String> n = new LinkedList<>();
+        n.add("A");
+        System.out.println(n);
+        n.add("B");
+        System.out.println(n);
+        n.addLast("C");
+        System.out.println(n);
+        n.addFirst("D");
+        System.out.println(n);
+        n.add(2, "E");
+        System.out.println(n);
     }
 }
